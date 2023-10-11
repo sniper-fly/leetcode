0611bbec7e5d21f22913069c2fb2d14992ce4174
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2023-10-11 11:11:52 +0900

    10/11 triangle judge, consecutive num

---
+++ sql50/consecutive_num.sql
@@ -0,0 +1,9 @@
+select
+  distinct l1.num as ConsecutiveNums
+from
+  Logs l1
+  join Logs l2 on l1.id + 1 = l2.id
+  join Logs l3 on l1.id + 2 = l3.id
+where
+  l1.num = l2.num
+  and l2.num = l3.num
