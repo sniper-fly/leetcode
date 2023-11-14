0521f4947e518305b572f774ddcd3d6572d7550f
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2023-11-14 22:56:51 +0900

    promise race

--- js30/2637_promise_time_limit.ts
+++ js30/2637_promise_time_limit.ts
@@ -1,10 +1,26 @@
 type Fn = (...params: any[]) => Promise<any>;
 
+// function timeLimit(fn: Fn, t: number): Fn {
+//   return async function (...args) {
+//     const original = fn(...args);
+//     const onExceeded = new Promise((_res, rej) =>
+//       setTimeout(() => {
+//         rej("Time Limit Exceeded");
+//       }, t)
+//     );
+
+//     return Promise.race([original, onExceeded]);
+//   };
+// }
+
 function timeLimit(fn: Fn, t: number): Fn {
   return async function (...args) {
-    return new Promise
+    return new Promise((res, rej) => {
+      setTimeout(() => rej("Time Limit Exceeded"), t)
+      fn(...args).then(res).catch(rej)
+    })
   };
 }
 
-const limited = timeLimit((t) => new Promise((res) => setTimeout(res, t)), 100);
-limited(150).catch(console.log); // "Time Limit Exceeded" at t=100ms
+// const limited = timeLimit((t) => new Promise((res) => setTimeout(res, t)), 100);
+// limited(150).catch(console.log); // "Time Limit Exceeded" at t=100ms
