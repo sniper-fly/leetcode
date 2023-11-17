93e53c62d2d654c37d1eeb15e22bb39ce58e181c
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2023-11-18 00:38:14 +0900

    2627 debounce

---
+++ js30/2627_debounce.ts
@@ -0,0 +1,39 @@
+type F = (...args: number[]) => void;
+
+function debounce(fn: F, t: number): F {
+  let timerId: undefined | number = undefined;
+  return function (...args) {
+    // 現在記録されている fn を clearTimeout する
+    clearTimeout(timerId);
+
+    // t m秒後に fnをsetTimeoutする
+    timerId = Number(
+      setTimeout(() => {
+        fn(...args);
+      }, t)
+    );
+  };
+}
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
+
+// async function a(t: number) {
+//   return new Promise((resolve) => {
+//     setTimeout(resolve, t);
+//   });
+// }
+
+// async function main() {
+//   const timerId = setTimeout(() => {
+//     console.log("aa");
+//   }, 10);
+//   await a(100);
+//   clearTimeout(timerId);
+// }
+
+// main();
