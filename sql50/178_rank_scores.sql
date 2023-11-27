8b10e8b9fee04dc1ea058293ffeb341cce78ef55
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2023-11-27 12:52:46 +0900

    just self join tables

--- sql50/178_rank_scores.sql
+++ sql50/178_rank_scores.sql
@@ -1,7 +1,33 @@
 select
   score,
-  dense_rank() over(order by score desc) as "rank"
+  dense_rank() over(
+    order by
+      score desc
+  ) as "rank"
 from
   Scores
 order by
-  score desc
+  score desc;
+
+-- SELECT
+--   Score,
+--   convert(Rank, SIGNED) AS Rank
+-- FROM
+--   (
+--     SELECT
+--       Score,
+--       @rank :=CASE
+--         WHEN Score = @previous THEN @rank
+--         ELSE @rank + 1
+--       END AS Rank,
+--       @previous := Score
+--     FROM
+--       Scores,
+--       (
+--         SELECT
+--           @previous := -1,
+--           @rank := 0
+--       ) AS initial
+--     ORDER BY
+--       Score DESC
+--   ) A;
