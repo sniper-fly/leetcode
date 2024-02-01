31dff8915fb64f70509fb502e76d44532e956a80
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-02-01 13:54:54 +0900

    group count

---
+++ sql50/570_2.sql
@@ -0,0 +1,17 @@
+select
+  name
+from
+  Employee
+where
+  id in (
+    select
+      managerId
+    from
+      Employee
+    where
+      managerId is not null
+    group by
+      managerId
+    having
+      count(managerId) >= 5
+  )
