64a565828738183703cae098f5b6de2b2667c0b8
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-02-13 10:14:05 +0900

    union all

---
+++ sql50/602_2.sql
@@ -0,0 +1,22 @@
+select
+  id,
+  count(*) as num
+from
+  (
+    select
+      requester_id as id
+    from
+      RequestAccepted
+    union
+    all
+    select
+      accepter_id as id
+    from
+      RequestAccepted
+  ) q
+group by
+  id
+order by
+  num desc
+limit
+  1
