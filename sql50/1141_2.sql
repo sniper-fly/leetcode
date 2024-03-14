60646ac5895c4ef8bc13f1c371d309cf4a0ea00b
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-14 12:43:14 +0900

    date_sub は遅い。leetcode 向けに高速化をするならマジックナンバーを使う

---
+++ sql50/1141_2.sql
@@ -0,0 +1,14 @@
+select
+  activity_date as day,
+  count(distinct user_id) as active_users
+from
+  Activity
+where
+  -- activity_date between DATE_SUB('2019-07-27', INTERVAL 29 DAY)
+  -- and '2019-07-27'
+  -- activity_date > '2019-06-27'
+  -- and activity_date <= '2019-07-27'
+  activity_date between '2019-06-28'
+  and '2019-07-27'
+group by
+  day
