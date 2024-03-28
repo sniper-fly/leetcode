31ba06c76671cfbbc703d2ebb8b09d05e92bbb99
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-03-28 10:39:29 +0900

    最もシンプルな回答

--- sql50/1174_2.sql
+++ sql50/1174_2.sql
@@ -15,4 +15,22 @@ from
       Delivery
   ) q
 where
-  ranking = 1
+  ranking = 1;
+
+select
+  round(
+    avg((order_date = customer_pref_delivery_date) * 100),
+    2
+  ) as immediate_percentage
+from
+  Delivery
+where
+  (customer_id, order_date) in (
+    select
+      customer_id,
+      min(order_date) as first_date
+    from
+      Delivery
+    group by
+      customer_id
+  )
