1db366d46cbd6e0b22b5de695712661293a5da53
Ryo Nakai <50983271+sniper-fly@users.noreply.github.com>
2024-05-01 09:49:12 +0900

    sum(ifnull pattern

---
+++ sql50/1407_2.sql
@@ -0,0 +1,11 @@
+select
+  u.name,
+  sum(ifnull(r.distance, 0)) as travelled_distance
+from
+  Users u
+  left join Rides r on u.id = r.user_id
+group by
+  r.user_id
+order by
+  travelled_distance desc,
+  name asc
